741d0fccba15ff90daf72a13673124a2ea45bbf1
Paolo Posso <pvictorsys@gmail.com>
2021-02-13 23:49:54 -0300

    add tests on main

--- src/main.rs
+++ src/main.rs
@@ -5,53 +5,30 @@ use rustcointlib::*;
 mod block_chain;
 use block_chain::Blockchain;
 use ed25519_dalek::Keypair;
+use wallet::Wallet;
 
 mod wallet;
 
 fn main() {
-    // let first_block = Block::new("0".to_owned(), vec![Transaction {
-    //   sender: String::from("Ryan"),
-    //   receiver: String::from("Dan"),
-    //   amount: 2000.00
-    // }]);
+    let mut blockchain = Blockchain::new();
 
-    // let hash1 = first_block.hash;
+    let paolo_key = Wallet::new();
+    let jorge_key = Wallet::new();
+    let miner_key = Wallet::new();
 
-    // let sec_block = Block::new(hash1.to_owned(), vec![Transaction {
-    //     sender: String::from("Ryan"),
-    //     receiver: String::from("Dan"),
-    //     amount: 2000.00
-    // }]);
+    let mut fist_transaction = Transaction {
+        sender: Some(paolo_key.public),
+        receiver: Some(jorge_key.public),
+        amount: 2000.0,
+        signature: None,
+    };
+    fist_transaction.sign_transaction(Keypair {
+        public: paolo_key.public,
+        secret: paolo_key.secret
+    });
+    blockchain.add_new_transaction(fist_transaction);
+    blockchain.mine_unmined_transactions(miner_key.public);
 
-    // println!("{:#?}", sec_block);
-    // println!("{:#?}", first_block);
-
-    // let mut blockchain = Blockchain::new();
-    // let genesis_block = Block::new(vec![Transaction {
-    //     amount:2000.0,
-    //     sender: String::from("Ryan"),
-    //     receiver: String::from("Dan"),
-    // }]);
-    // let first_block = Block::new(vec![Transaction {
-    //     amount:1000.0,
-    //     sender: String::from("Paolo"),
-    //     receiver: String::from("Jorge"),
-    // }]);
-    // let second_block = Block::new(vec![Transaction {
-    //     amount:1000.0,
-    //     sender: String::from("Thayna"),
-    //     receiver: String::from("Paolo"),
-    // }]);
-
-    // let mut blockchain = Blockchain::new();
-    // blockchain.add_new_transaction(Transaction {sender: String::from("Paolo"), receiver: String::from("Victor"), amount: 1000.0});
-    // blockchain.add_new_transaction(Transaction {sender: String::from("Jorge"), receiver: String::from("Paolo"), amount: 1000.0});
-    // blockchain.add_new_transaction(Transaction {sender: String::from("Thayna"), receiver: String::from("Paolo"), amount: 1000.0});
-
-    // blockchain.mine_unmined_transactions("First Miner".to_owned());
-
-    // println!("{:#?}", blockchain);
-    // println!("Valid: {}", blockchain.is_valid_chain());
-
-
+    print!("{}", blockchain.is_valid_chain());
+    print!("{:#?}", blockchain);
 }
